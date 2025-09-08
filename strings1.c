5daac35280001f16c9000c93593a2e16e2ddb25d
MOUAINE <fatihamouaine1@gmail.com>
2025-09-08 12:22:11 +0100

    Ajout strings

---
+++ strings1.c
@@ -0,0 +1,62 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main (){
+    //----------------------------Challenge 1 : Affichage d'une Chaîne
+    /*char chaine[10];
+    printf("Entrer votre nom :");
+    gets(chaine);
+    puts(chaine);*/
+    //----------------------------Challenge 2 : Longueur de la Chaîne
+    /*char chaine[] = "Bonjour";
+    int cmp = 0;
+    while (chaine[cmp] != '\0') {
+        cmp++;
+    }
+    printf("Longueur: %d\n", cmp);*/
+    //---------------------------Challenge 3 : Concaténation de Chaînes
+    /*char c[]="FATIHA";
+    char d[10]="MOUAINE ";
+    strcat(d,c);     // la concatenation
+    puts(d);*/
+    //---------------------------Challenge 4 : Comparaison de Chaînes
+    /*char a[]="Fatiha";
+    char b[]="Fatiha";
+    char c[]="fatiha";
+    char d[]="Mouaine";
+    printf("%d \n",strcmp(a,b));          //res : 0
+    printf("%d \n",strcmp(a,c));          //res : -1 ( A < a )
+    printf("%d \n",strcmp(c,d));*/          //res : 1 ( m > l)
+    //---------------------------Challenge 5 : Inversion de Chaîne
+    /*char e[]="fatiha";
+    //strrev: renvoyer l'inverse d'une chaine
+    printf("%s",strrev(e));*/
+    //---------------------------???Challenge 6 : Compte des Occurrences d'un Caractère
+    /*char a[]="hello world";
+    printf("%s",strchr(a,'o'));*/
+    //---------------------------Challenge 7 : Conversion en Majuscules
+    /*char a[]="fatiha";
+    printf("%s",strupr(a));
+    //---------------------------Challenge 8 : Conversion en Minuscules
+    char b[]="FATIHA";
+    printf("%s",strlwr(b));*/
+    //----------------------------Challenge 9 : Suppression des Espaces
+    /*char chaine[100], res[100];
+    int i = 0, j = 0;
+
+    printf("Entrez une chaine de caracteres: ");
+    fgets(chaine, sizeof(chaine), stdin);
+
+    while (chaine[i] != '\0') {
+        if (chaine[i]!= ' ') {
+            res[j] = chaine[i];
+            j++;
+        }
+        i++;
+    }
+    res[j] = '\0';
+    printf("Chaine sans espaces: %s\n", res);*/
+
+
+    /*return 0;
+}*/
